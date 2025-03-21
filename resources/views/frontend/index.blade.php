11edda358e6caa98703d1697dce61ede016b341a
malir2 <maliramzan314@gmail.com>
2025-03-21 13:30:29 +0500

    Update styles and scripts for Slick Slider integration and enhance service card responsiveness

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -37,48 +37,246 @@
           </div>
         </div>
         <div class="mt-5">
-            <div class="d-flex flex-3 gap-4">
-                <div class="py-5 bg-black service-card">
-                        <h4 class="text-white font-600 font-lato">Airport Limo Service</h4>
-                        <div class="position-relative mt-4 card-border">
-                            <div class="position-absolute card-inner-border"></div>
-                        </div>
-                        <div class="d-flex flex-column inner-section">
-                            <p class="text-white mt-4 card-content font-lato">Travel to and from Calgary International Airport (YYC) in luxury and comfort with our professional and proficient chauffeurs.</p>
-                            <div class="mt-auto">
-                                <a href="#" class="btn-4-white rounded"><span>Book Now</span></a>
-                            </div>
-                        </div>
-                    
-                </div>
-                <div class="py-5 bg-black service-card">
-                    <h4 class="text-white font-600 font-lato">Airport Limo Service</h4>
+          <div class="d-flex flex-wrap gap-4 custom-flex">
+            <div class="py-5 bg-black service-card">
+                    <h4 class="text-white font-600 font-lato pointer">Airport Limo Service</h4>
                     <div class="position-relative mt-4 card-border">
                         <div class="position-absolute card-inner-border"></div>
                     </div>
                     <div class="d-flex flex-column inner-section">
                         <p class="text-white mt-4 card-content font-lato">Travel to and from Calgary International Airport (YYC) in luxury and comfort with our professional and proficient chauffeurs.</p>
                         <div class="mt-auto">
-                            <a href="#" class="btn-4-white rounded"><span>Book Now</span></a>
+                            <a href="#" class="btn-4-white rounded pointer"><span>Book Now</span></a>
                         </div>
                     </div>
+                
+            </div>
+            <div class="py-5 bg-black service-card">
+                <h4 class="text-white font-600 font-lato">Canmore/Banff/Lake Louise</h4>
+                <div class="position-relative mt-4 card-border">
+                    <div class="position-absolute card-inner-border"></div>
                 </div>
-                <div class="py-5 bg-black service-card">
-                    <h4 class="text-white font-600 font-lato">Airport Limo Service</h4>
-                    <div class="position-relative mt-4 card-border">
-                        <div class="position-absolute card-inner-border"></div>
-                    </div>
-                    <div class="d-flex flex-column inner-section">
-                        <p class="text-white mt-4 card-content font-lato">Travel to and from Calgary International Airport (YYC) in luxury and comfort with our professional and proficient chauffeurs.</p>
-                        <div class="mt-auto">
-                            <a href="#" class="btn-4-white rounded"><span>Book Now</span></a>
-                        </div>
+                <div class="d-flex flex-column inner-section">
+                    <p class="text-white mt-4 card-content font-lato">Black Sedan has experienced chauffeurs that can guide you through the scenic Rocky ountains in your cozy and comfortable limo on your way to one of the world’s most popular vacation destinations.</p>
+                    <div class="mt-auto">
+                        <a href="#" class="btn-4-white rounded"><span>Book Now</span></a>
                     </div>
                 </div>
             </div>
+            <div class="py-5 bg-black service-card">
+                <h4 class="text-white font-600 font-lato">City Trip</h4>
+                <div class="position-relative mt-4 card-border">
+                    <div class="position-absolute card-inner-border"></div>
+                </div>
+                <div class="d-flex flex-column inner-section">
+                    <p class="text-white mt-4 card-content font-lato">Tour the city of Calgary and its famous landmarks in our luxurious and clean cars, where our experienced chauffeurs can take you to your hotel or residence.</p>
+                    <div class="mt-auto">
+                        <a href="#" class="btn-4-white rounded"><span>Book Now</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="py-5 bg-black service-card">
+                <h4 class="text-white font-600 font-lato">Corporate Car Service</h4>
+                <div class="position-relative mt-4 card-border">
+                    <div class="position-absolute card-inner-border"></div>
+                </div>
+                <div class="d-flex flex-column inner-section">
+                    <p class="text-white mt-4 card-content font-lato">Black Sedan understands the importance of time and offers priority limo service to existing and long-lasting corporate partners. Experience executive and exclusive limo service on your next corporate get together or business meeting by using our services.</p>
+                    <div class="mt-auto">
+                        <a href="#" class="btn-4-white rounded"><span>Book Now</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="py-5 bg-black service-card">
+                <h4 class="text-white font-600 font-lato">Special Events</h4>
+                <div class="position-relative mt-4 card-border">
+                    <div class="position-absolute card-inner-border"></div>
+                </div>
+                <div class="d-flex flex-column inner-section">
+                    <p class="text-white mt-4 card-content font-lato">Add grace to all your important events with Black Sedan. We make sure the transportation is effortless and your weddings, graduation, anniversaries and more are memorable.</p>
+                    <div class="mt-auto">
+                        <a href="#" class="btn-4-white rounded"><span>Book Now</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="py-5 bg-black service-card">
+                <h4 class="text-white font-600 font-lato">Surrounding Areas</h4>
+                <div class="position-relative mt-4 card-border">
+                    <div class="position-absolute card-inner-border"></div>
+                </div>
+                <div class="d-flex flex-column inner-section">
+                    <p class="text-white mt-4 card-content font-lato">We cater to all your needs. Travel to and from Calgary to other locations such as Lethbridge, Edmonton, Red Deer, Medicine Hat, and more!</p>
+                    <div class="mt-auto">
+                        <a href="#" class="btn-4-white rounded"><span>Book Now</span></a>
+                    </div>
+                </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+
+    {{-- Fleet Section --}}
+    <div class="py-5 container-custom fleet-section">
+      <div class="mb-3 mt-5 heading-border"></div>
+      <h1 class="mb-0 text-black text-center">Our Fleet</h1>
+      <div class="mt-4 fleet-cards">
+        <div class="fleet-card">
+          <div class="p-3 fleet-card-inner">
+            <div class="position-relative fleet-card-image">
+              <div class="image-shade"></div>
+              <img src="{{ asset("frontend/images/car1.webp") }}" alt="Car 1"  />
+            </div>
+            <div class="p-3">
+              <h4>Lincoln Aviator Sedan</h4>
+              <ul class="px-3 mb-0">
+                <li>3 pieces of luggage</li>
+                <li>up-to 3 passengers maximum</li>
+              </ul>
+              <p class="mb-0 fw-bold">...</p>
+              <div class="mt-4">
+                <a href="#" class="btn-4 rounded fleet-btn"><span class="fw-bold">Book Now</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="fleet-card-border-bottom"></div>
+        </div>
+        <div class="fleet-card">
+          <div class="p-3 fleet-card-inner">
+            <div class="position-relative fleet-card-image">
+              <div class="image-shade"></div>
+              <img src="{{ asset("frontend/images/car1.webp") }}" alt="Car 1"  />
+            </div>
+            <div class="p-3">
+              <h4>Lincoln Aviator Sedan</h4>
+              <ul class="px-3 mb-0">
+                <li>3 pieces of luggage</li>
+                <li>up-to 3 passengers maximum</li>
+              </ul>
+              <p class="mb-0 fw-bold">...</p>
+              <div class="mt-4">
+                <a href="#" class="btn-4 rounded fleet-btn"><span class="fw-bold">Book Now</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="fleet-card-border-bottom"></div>
+        </div>
+        <div class="fleet-card">
+          <div class="p-3 fleet-card-inner">
+            <div class="position-relative fleet-card-image">
+              <div class="image-shade"></div>
+              <img src="{{ asset("frontend/images/car1.webp") }}" alt="Car 1"  />
+            </div>
+            <div class="p-3">
+              <h4>Lincoln Aviator Sedan</h4>
+              <ul class="px-3 mb-0">
+                <li>3 pieces of luggage</li>
+                <li>up-to 3 passengers maximum</li>
+              </ul>
+              <p class="mb-0 fw-bold">...</p>
+              <div class="mt-4">
+                <a href="#" class="btn-4 rounded fleet-btn"><span class="fw-bold">Book Now</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="fleet-card-border-bottom"></div>
+        </div>
+        <div class="fleet-card">
+          <div class="p-3 fleet-card-inner">
+            <div class="position-relative fleet-card-image">
+              <div class="image-shade"></div>
+              <img src="{{ asset("frontend/images/car1.webp") }}" alt="Car 1"  />
+            </div>
+            <div class="p-3">
+              <h4>Lincoln Aviator Sedan</h4>
+              <ul class="px-3 mb-0">
+                <li>3 pieces of luggage</li>
+                <li>up-to 3 passengers maximum</li>
+              </ul>
+              <p class="mb-0 fw-bold">...</p>
+              <div class="mt-4">
+                <a href="#" class="btn-4 rounded fleet-btn"><span class="fw-bold">Book Now</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="fleet-card-border-bottom"></div>
+        </div>
+        <div class="fleet-card">
+          <div class="p-3 fleet-card-inner">
+            <div class="position-relative fleet-card-image">
+              <div class="image-shade"></div>
+              <img src="{{ asset("frontend/images/car1.webp") }}" alt="Car 1"  />
+            </div>
+            <div class="p-3">
+              <h4>Lincoln Aviator Sedan</h4>
+              <ul class="px-3 mb-0">
+                <li>3 pieces of luggage</li>
+                <li>up-to 3 passengers maximum</li>
+              </ul>
+              <p class="mb-0 fw-bold">...</p>
+              <div class="mt-4">
+                <a href="#" class="btn-4 rounded fleet-btn"><span class="fw-bold">Book Now</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="fleet-card-border-bottom"></div>
+        </div>
+        <div class="fleet-card">
+          <div class="p-3 fleet-card-inner">
+            <div class="position-relative fleet-card-image">
+              <div class="image-shade"></div>
+              <img src="{{ asset("frontend/images/car1.webp") }}" alt="Car 1"  />
+            </div>
+            <div class="p-3">
+              <h4>Lincoln Aviator Sedan</h4>
+              <ul class="px-3 mb-0">
+                <li>3 pieces of luggage</li>
+                <li>up-to 3 passengers maximum</li>
+              </ul>
+              <p class="mb-0 fw-bold">...</p>
+              <div class="mt-4">
+                <a href="#" class="btn-4 rounded fleet-btn"><span class="fw-bold">Book Now</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="fleet-card-border-bottom"></div>
+        </div>
+      </div>
+      <div class="py-5"></div>
+    </div>
   </div>
 
+  <script>
+    $(document).ready(function () {
+      $('.fleet-cards').slick({
+    slidesToShow: 3, // Show 3 cards by default
+    slidesToScroll: 1, // Scroll one card at a time
+    infinite: false, // Infinite looping
+    draggable: true, // Enable dragging
+    arrows: true, // Show navigation buttons
+    prevArrow: '<button type="button" class="slick-prev"><span class="fa-solid fa-angle-left arrows"></span></button>',
+    nextArrow: '<button type="button" class="slick-next"><span class="fa-solid fa-angle-right arrows"></span></button>',
+    responsive: [
+        {
+            breakpoint: 767, // On screens smaller than 767px
+            settings: {
+                slidesToShow: 2, // Show 2 cards
+            },
+        },
+        {
+            breakpoint: 575, // On screens smaller than 575px
+            settings: {
+                slidesToShow: 1, // Show 1 card
+            },
+        },
+    ],
+});
+    });
+  </script>
 @endsection
--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -21,11 +21,14 @@
 
     <!-- Font Awsome -->
     <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
       rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     />
 
-    
+    {{-- Slick Slider --}}
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" />
 
     {{-- jQuery CDN --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -40,6 +43,9 @@
     {{-- Bootstrap javascript --}}
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+{{-- Slick Slider --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
 
 <script src="{{ asset('frontend/js/index.js') }}"></script>
 </body>
